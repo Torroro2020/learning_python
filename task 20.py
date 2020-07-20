9e7ba0448cd4c5a3f60b5f25e7dec4a749354de5
79181 <tomeartur@yandex.ru>
2020-07-20 22:02:51 +0300

    20.07.2020 22:00

--- task 20.py
+++ task 20.py
@@ -2,6 +2,6 @@ list = [int(i) for i in input().split()]
 cnt = len(list)
 if cnt > 1:
     for i in range(cnt):
-        print(list[i-1]+ list[(i+1) % cnt], end=' ')
+        print(list[i-1] + list[(i+1) % cnt], end=' ')
 else:
     print(*list)
